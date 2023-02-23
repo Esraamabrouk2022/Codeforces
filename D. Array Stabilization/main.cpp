3b95df95643f730d0ccfba8419699bebfcfb4b00
Esraa Mabrouk <117677185+Esraamabrouk2022@users.noreply.github.com>
2023-02-23 21:52:54 +0200

    https://codeforces.com/group/3nQaj5GMG5/contest/372026/problem/D

---
+++ D. Array Stabilization/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+void fast()
+{
+    cin.tie(0);
+    cout.tie(0);
+}
+int main()
+{
+    fast();
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)cin>>arr[i];
+    sort(arr,arr+n);
+    int mi=min(arr[n-1]-arr[1],arr[n-2]-arr[0]);
+    cout<<mi;
+
+    return 0;
+}
